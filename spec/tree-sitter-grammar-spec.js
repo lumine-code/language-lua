41eaa22d988ecba24998684e5e0e18a0dbd6e2ce
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-09 18:54:19 +0200

    Complete the Lumine ecosystem rebrand

--- spec/tree-sitter-grammar-spec.js
+++ spec/tree-sitter-grammar-spec.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 describe("Lua Tree-sitter grammar", () => {
   beforeEach(async () => {
-    await atom.packages.activatePackage("language-lua");
+    await lumine.packages.activatePackage("language-lua");
   });
 
   it("tokenizes the fixture", async () => {
